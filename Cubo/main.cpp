c47d350e3bbb7536316d6f1b93171887a46d551b
augustoaccorsi <augusto.acorsi@gmail.com>
2019-10-02 20:20:09 -0300

    remove unused code

--- Cubo/main.cpp
+++ Cubo/main.cpp
@@ -303,9 +303,6 @@ void loadVertices(Mesh* mesh) {
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
