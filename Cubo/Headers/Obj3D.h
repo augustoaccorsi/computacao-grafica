6e1c20b7ad4b8e8fd23991e5a8f5914a4a704e9e
Arthur TC <arthurtcabral@gmail.com>
2019-10-27 17:46:18 -0300

    :construction: adicionar ajustes obj3d

--- Cubo/Headers/Obj3D.h
+++ Cubo/Headers/Obj3D.h
@@ -5,9 +5,26 @@
 
 class Obj3D {
 public:
-	Mesh* obj;
+	Mesh* mesh;
 	vector<Material*> materials;
+	glm::mat4 ModelMatrix;
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scale;
 
+	void Inicializar() {
+		this->position = glm::vec3(0.1f, 0.1f, 0.1f);
+		this->rotation = glm::vec3(0.0f);
+		this->scale = glm::vec3(1.f);
+
+		this->ModelMatrix = glm::mat4(1.0f);
+		this->ModelMatrix = glm::translate(this->ModelMatrix, this->position);
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.x), glm::vec3(1.f, 0.f, 0.f));
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.y), glm::vec3(0.f, 1.f, 0.f));
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.z), glm::vec3(0.f, 0.f, 1.f));
+		this->ModelMatrix = glm::scale(this->ModelMatrix, this->scale);
+
+	}
 
 	Mesh* Obj3D::processObj(const string filename) {
 		Mesh* mesh = readOBJ(filename);
